e99380efbbfa606dd37fc35e26b1f0f4898d7fa1
Yonela Johannes <69614136+Yonela-Johannes@users.noreply.github.com>
2022-04-03 11:47:21 +0200

    Projects button

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const about = document.querySelector('.about')
 const frontEnd = document.querySelector('.front-end')
 const backEnd = document.querySelector('.back-end')
+const next = document.querySelector('.next')
 // details containers
 const aboutDetails = document.querySelector('.about-details')
 const frontEndDetails = document.querySelector('.frontend-details')
@@ -18,7 +19,7 @@ about.addEventListener('click', () => {
     aboutDetails.classList.remove('hide')
     frontEndDetails.classList.add('hide')
     backEndDetails.classList.add('hide')
-
+    next.classList.add('hide')
 });
 
 frontEnd.addEventListener('click', () => {
@@ -31,6 +32,7 @@ frontEnd.addEventListener('click', () => {
     frontEndDetails.classList.remove('hide')
     aboutDetails.classList.add('hide')
     backEndDetails.classList.add('hide')
+    next.classList.remove('hide')
 });
 
 backEnd.addEventListener('click', () => {
@@ -43,4 +45,5 @@ backEnd.addEventListener('click', () => {
     backEndDetails.classList.remove('hide')
     aboutDetails.classList.add('hide')
     frontEndDetails.classList.add('hide')
+    next.classList.remove('hide')
 })
