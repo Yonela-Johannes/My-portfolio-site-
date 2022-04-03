58a894a3ab329185678f142f681c902d574e88ea
Yonela Johannes <69614136+Yonela-Johannes@users.noreply.github.com>
2022-04-03 21:07:56 +0200

    buttons hide toggle

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const about = document.querySelector('.about')
 const frontEnd = document.querySelector('.front-end')
 const backEnd = document.querySelector('.back-end')
 const next = document.querySelector('.next')
+const showProjects = document.querySelector('.view-projects')
+const hideProjects = document.querySelector('.hide-projects')
 // details containers
 const aboutDetails = document.querySelector('.about-details')
 const frontEndDetails = document.querySelector('.frontend-details')
@@ -46,4 +48,18 @@ backEnd.addEventListener('click', () => {
     aboutDetails.classList.add('hide')
     frontEndDetails.classList.add('hide')
     next.classList.remove('hide')
-})
+});
+
+showProjects.addEventListener('click', () => {
+    frontEndDetails.classList.remove('hide')
+    backEndDetails.classList.remove('hide')
+    hideProjects.classList.remove('hide')
+    showProjects.classList.add('hide')
+});
+
+hideProjects.addEventListener('click', () => {
+    frontEndDetails.classList.add('hide')
+    backEndDetails.classList.add('hide')
+    showProjects.classList.remove('hide')
+    hideProjects.classList.add('hide')
+});
